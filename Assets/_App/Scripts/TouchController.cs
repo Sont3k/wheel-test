3492dc325370ee2d7091469249e129326c618bf6
Sont3k <markus.residence@gmail.com>
2023-07-25 22:39:40 +0300

    feat: implement swipes for rotation

--- Assets/_App/Scripts/TouchController.cs
+++ Assets/_App/Scripts/TouchController.cs
@@ -4,16 +4,25 @@ namespace _App.Scripts
 {
     public class TouchController : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
+        [Header("References")]
+        [SerializeField] private Transform _spinningCircle;
 
-        // Update is called once per frame
-        void Update()
+        [Header("Parameters")]
+        [SerializeField] private float _rotationSpeed;
+
+        private Touch _touch;
+        private Vector2 _touchPosition;
+        private Quaternion _rotation;
+
+        private void Update()
         {
-        
+            if (Input.touchCount <= 0) return;
+
+            _touch = Input.GetTouch(0);
+            if (_touch.phase != TouchPhase.Moved) return;
+
+            _rotation = Quaternion.Euler(0f, 0f, -_touch.deltaPosition.x * _rotationSpeed);
+            _spinningCircle.transform.rotation *= _rotation;
         }
     }
 }
